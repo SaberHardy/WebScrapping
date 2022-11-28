21084a96305c1d8a4a3fd7ee61034e67999a44a7
SaberHardy <samirsamado5@gmail.com>
2022-11-28 18:24:07 +0100

    prblm of space in title tag

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from urllib.request import urlopen
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
+url = "http://olympus.realpython.org/profiles/poseidon"
 
 page = urlopen(url)
 html_bytes = page.read()
 
-html_object = html_bytes.decode("utf-8")
-title_index = html_object.find('title') + len("<title>")
+html_page = html_bytes.decode("utf-8")
+start_title_index = html_page.find('title') + len("<title>")
 
-print(html_object)
-print(title_index)
+end_title_index = html_page.find("</title>")
+
+print(html_page)
+
+print("The Title starting from: ", start_title_index)
+print("The Title end in: ", end_title_index)
+
+print(f"Page Title is: {html_page[start_title_index: end_title_index]}")
